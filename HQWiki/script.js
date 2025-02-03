e75f5f1c29d896af9bba567eaabdcb5c29398e12
Tiago Lisboa <tiagolisboa2020@gmail.com>
2025-02-03 20:48:25 -0300

    organização de pastas

--- HQWiki/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cards = document.querySelectorAll('.card');
-  
-    cards.forEach(card => {
-      card.addEventListener('click', function() {
-        alert(`Você clicou na categoria: ${this.querySelector('.card-title').textContent}`);
-      });
-    });
-  });
